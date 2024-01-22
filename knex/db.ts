4e2e496dcbc4f86cd4b1116d88a57ec7fa050a08
P. S. Brannan <psbrannan@pm.me>
2024-01-22 17:13:34 -0600

    Make the config typesafe

--- knex/db.ts
+++ knex/db.ts
@@ -1,10 +1,6 @@
-import knex from 'knex';
+import knex, { Knex } from 'knex';
 import config from './knexfile';
 
-console.log('process.env.DATABASE_URL: ', process.env.DATABASE_URL);
-if (!config.development.connection || !config.development.connection.filename) {
-    throw new Error('No connection string provided in knexfile.ts or DATABASE_URL environment variable');
-}
-const db = knex(config.development)
+const db = knex(config.development as Knex.Config<any>)
 
 export default db;
